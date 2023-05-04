849783e90e0e8aa345df309dd1e3be29fd09fb19
Adaptist <adaptist1@gmail.com>
2023-05-04 11:54:47 +0300

    9.01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+let intervalId = null;
+
+startBtn.addEventListener('click', () => {
+  if (intervalId === null) {
+    intervalId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    startBtn.disabled = true;
+  }
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(intervalId);
+  intervalId = null;
+  startBtn.disabled = false;
+});
